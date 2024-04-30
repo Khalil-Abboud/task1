fa57af67b18ddf7cd48f2df0aa72e1df32b98556
Khalil-Abboud <khalil.ad.1998.98.com>
2024-04-30 17:07:38 +0300

    3rd task final

---
+++ SequenceGenerator/src/main/java/SequenceGen.java
@@ -0,0 +1,8 @@
+public class SequenceGen {
+        public static void sequenceGen(int[] myArray, int min, int max) {
+            for (int i = 0; i< myArray.length; i++){
+                int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
+                myArray[i]=random_int;
+            }
+        }
+    }
