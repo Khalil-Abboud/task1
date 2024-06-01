28a9051113ad878c073a2a4ff7c1728e018174a2
Khalil-Abboud <khalil.ad.1998.98.com>
2024-06-01 10:38:44 +0300

    4rd task final jenkins 2

--- SequenceGenerator/src/main/java/SequenceGen.java
+++ SequenceGenerator/src/main/java/SequenceGen.java
@@ -1,8 +1,12 @@
 public class SequenceGen {
         public static void sequenceGen(int[] myArray, int min, int max) {
+            test4();
             for (int i = 0; i< myArray.length; i++){
                 int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
                 myArray[i]=random_int;
             }
+        }
+        public static void test4(){
+            System.out.println("hello word");
         }
     }
